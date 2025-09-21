ac6e96b1a2a64af1016778bda6f2c0f8e28972f0
rbhagat32 <raghavbhagat32@gmail.com>
2025-09-22 00:10:00 +0530

    init

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -97,7 +97,7 @@ const SkeletonLoader = () => {
   return [...Array(11)].map((_, index) => (
     <div
       key={index}
-      className="mx-auto h-9 w-[280px] animate-pulse rounded-2xl bg-gray-800 px-3 py-2 text-gray-100 shadow-lg"
+      className="mx-auto h-9 w-[280px] animate-pulse rounded-2xl bg-gray-900 px-3 py-2 text-gray-100 shadow-lg"
     />
   ));
 };
