3a1e9defd97c9e669b13a23c536b369172125f4b
rbhagat32 <raghavbhagat32@gmail.com>
2025-09-21 23:54:29 +0530

    init

--- backend/src/config/redis.ts
+++ backend/src/config/redis.ts
@@ -24,11 +24,11 @@ sub.on("ready", () => {
 });
 
 pub.on("error", (err) => {
-  console.error("Redis Pub Error:", err);
+  console.error("Redis Publisher Error:", err);
 });
 
 sub.on("error", (err) => {
-  console.error("Redis Sub Error:", err);
+  console.error("Redis Subscriber Error:", err);
 });
 
 export { pub, sub };
