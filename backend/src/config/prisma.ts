8c6a7b58c3bb2fd3dc65fa104288511ddf0e8f94
rbhagat32 <raghavbhagat32@gmail.com>
2025-09-22 22:07:08 +0530

    init

--- backend/src/config/prisma.ts
+++ backend/src/config/prisma.ts
@@ -4,12 +4,21 @@ const prisma = new PrismaClient({
   log: ["query", "info", "warn", "error"],
 });
 
-prisma.$connect().then(() => {
-  console.log("Connected to PostgreSQL !");
-});
+prisma
+  .$connect()
+  .then(() => {
+    console.log("Connected to PostgreSQL !");
+  })
+  .catch((e) => {
+    console.error("Error connecting to PostgreSQL: ", e);
+  });
 
 prisma.$on("error", (e) => {
   console.error("Prisma error: ", e);
+});
+
+process.on("beforeExit", async () => {
+  await prisma.$disconnect();
 });
 
 export { prisma };
