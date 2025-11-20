fbf598ad859317ef6cefbae2cf4705ac81418775
rbhagat32 <raghavbhagat32@gmail.com>
2025-11-20 15:26:41 +0530

    init

--- frontend/src/components/core/navbar.tsx
+++ frontend/src/components/core/navbar.tsx
@@ -2,7 +2,6 @@ export function NavBar() {
   return (
     <div className="flex flex-col gap-2 border-b border-gray-800 bg-gray-900/50 p-4 backdrop-blur-sm">
       <h1 className="text-center text-xl font-medium">Scalable Chat App</h1>
-
       <p className="text-center text-xs font-light">
         Next.js, Express.js, PostgreSQL, Prisma, Socket.io, Redis, Kafka,
         Docker, Nginx
