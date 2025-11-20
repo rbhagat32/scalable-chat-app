530ad3d356c29b3be902e9efecf01024ab630b6f
rbhagat32 <raghavbhagat32@gmail.com>
2025-11-20 14:58:53 +0530

    init

--- backend/src/routes/auth.ts
+++ backend/src/routes/auth.ts
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import {  signUp, login, logout, getLoggedInUser } from "@/controllers/auth.js";
+import { signUp, login, logout, getLoggedInUser } from "@/controllers/auth.js";
 import { multerUpload } from "@/config/multer.js";
 import { isLoggedIn } from "@/middlewares/auth.js";
 
